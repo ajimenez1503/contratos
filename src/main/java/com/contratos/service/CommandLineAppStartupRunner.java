44f772681ad5aadb9887e774d8151faddcd90a81
Antonio Jimenez <ajimenez@rti.com>
2022-06-21 13:11:55 +0200

    add predefined data

---
+++ src/main/java/com/contratos/service/CommandLineAppStartupRunner.java
@@ -0,0 +1,39 @@
+package com.contratos.service;
+
+import com.contratos.model.Category;
+import com.contratos.model.Institute;
+import com.contratos.model.Province;
+import com.contratos.respository.CategoryRepository;
+import com.contratos.respository.InstituteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+@Component
+public class CommandLineAppStartupRunner implements CommandLineRunner {
+    private static final Logger LOG =
+            LoggerFactory.getLogger(CommandLineAppStartupRunner.class);
+
+    private InstituteRepository instituteRepository;
+
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public CommandLineAppStartupRunner(InstituteRepository instituteRepository, CategoryRepository categoryRepository) {
+        this.instituteRepository = instituteRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String...args) throws Exception {
+        LOG.info("Initialize categories and institutes tables");
+        categoryRepository.save(new Category("TCAE", "Auxiliar de Enfermería"));
+        categoryRepository.save(new Category("DUE", "Enfermería"));
+        categoryRepository.save(new Category("TCAE", "Tecnico en Ambulancia"));
+        categoryRepository.save(new Category("FEA", "Facultativo Especialista Adjunto"));
+
+        instituteRepository.save(new Institute(0L, "Centro de salud Abla", "AV SANTOS MARTIRES, 22 04510 ABLA Abla Almería", Province.ALMERIA));
+        instituteRepository.save(new Institute(1L, "Consultorio Armuña de Almanzora", "CL ERAS, 18 04888 ARMUÑA DE ALMANZORA Armuña de Almanzora Almería", Province.ALMERIA));
+    }
+}
